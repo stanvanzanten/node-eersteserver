4867fdf36ee51893916ba2e16e1b649dbb629450
Stan van Zanten <stanzanten@hotmail.com>
2017-04-20 11:38:11 +0200

    Hier mijn boodschap1

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+http.createServer(function(request, response){
+	console.log('Er was een request.');
+	response.writeHead(200, {'Content-Type': 'text/json'});
+	var json = JSON.stringify({
+		tekst:"Dit is JSON"
+	});
+	response.end(json);
+}).listen(3000);
+
+console.log('De server luistert op port 3000');
